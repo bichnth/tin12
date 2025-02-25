0a7f099b7ff405461e9924e23d4b3e12a485792a
bichnth <132417455+bichnth@users.noreply.github.com>
2025-02-25 07:26:19 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 function login()
 {
+    document.getElementById(compiler).style.display="block";
     let a = first.value
     let b = password.value
     if (a == "abc" && b == "123")
     {
         window.open("home2.html")
-        document.getElementById(compiler).style.display="block";
+        
     }
     else
         document.write("Không hợp lệ")
